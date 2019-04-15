d2260e198da10bbea0a26e99c4823c8eb47b43df
Danziel <gruesucdaniel006@gmail.com>
2019-04-15 16:55:37 +0300

    The finall version `

--- js/pres.js
+++ js/pres.js
@@ -1,16 +1,16 @@
 var hovered = false;
 
-$(document).ready(function() {
-	$('.pres').click(function(){
+$(document).ready(function() { //Checks for the complete loading of the site
+	$('.pres').click(function(){ //Checks for the clicking action on a div that has the class pres
 
-		if($(this).hasClass('clicked')){
-			$('.pres' + $(this).attr('rel') +  ' img').animate({
+		if($(this).hasClass('clicked')){ //Verifies if the clicked item has a certain class, this is used to verify the clicked state of the item
+			$('.pres' + $(this).attr('rel') +  ' img').animate({ //This animates the growth of  the item
 			width: '90',
 			height: 170,
 			'box-shadow': '10px 4px 43px 2000px rgba(0,0,0,0.75)',
 		
 		});
-			$(this).removeClass('clicked');
+			$(this).removeClass('clicked'); //Gets rid of the class
 		}
 		else{
 		$('.pres' + $(this).attr('rel') +  ' img').animate({
